828913297fa79dcd4e4667ecb63d233d8cdeeddc
Thunyaporn <thunthunyaporn@gmail.com>
2016-10-12 16:47:09 +0700

    add case rigth

--- CAPTCHA/captcha/spec/captcha.spec.js
+++ CAPTCHA/captcha/spec/captcha.spec.js
@@ -114,6 +114,92 @@ function leftoperand(PatternValue,LeftoperandValue) {
 
 
 
-function rigthoperand() {
+function rigthoperand(PatternValue,RigthoperandValue) {
+
+  if(PatternValue === 1){
+    if(RigthoperandValue === 0){
+      left = "zero";
+      return left;
+    }
+    else if(RigthoperandValue === 1){
+      left = "one";
+      return left;
+    }
+    else if(RigthoperandValue === 2){
+      left = "two";
+      return left;
+    }
+    else if(RigthoperandValue === 3){
+      left = "three";
+      return left;
+    }
+    else if(RigthoperandValue === 4){
+      left = "four";
+      return left;
+    }
+    else if(RigthoperandValue === 5){
+      left = "five";
+      return left;
+    }
+    else if(RigthoperandValue === 6){
+      left = "six";
+      return left;
+    }
+    else if(RigthoperandValue === 7){
+      left = "seven";
+      return left;
+    }
+    else if(RigthoperandValue === 8){
+      left = "eight";
+      return left;
+    }
+    else if(RigthoperandValue === 9){
+      left = "nine";
+      return left;
+    }
+  }
+
+  if(PatternValue === 2 ){
+    if(RigthoperandValue === 0){
+      left = "0";
+      return left;
+    }
+    else if(RigthoperandValue === 1){
+      left = "1";
+      return left;
+    }
+    else if(RigthoperandValue === 2){
+      left = "2";
+      return left;
+    }
+    else if(RigthoperandValue === 3){
+      left = "3";
+      return left;
+    }
+    else if(RigthoperandValue === 4){
+      left = "4";
+      return left;
+    }
+    else if(RigthoperandValue === 5){
+      left = "5";
+      return left;
+    }
+    else if(RigthoperandValue === 6){
+      left = "6";
+      return left;
+    }
+    else if(RigthoperandValue === 7){
+      left = "7";
+      return left;
+    }
+    else if(RigthoperandValue === 8){
+      left = "8";
+      return left;
+    }
+    else if(RigthoperandValue === 9){
+      left = "9";
+      return left;
+    }
+  }
 
 }
