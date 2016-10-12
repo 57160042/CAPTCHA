71f62ee281d8733b321b2f96ca046a1c456ef76f
57160042 <57160042@go.buu.ac.th>
2016-10-12 16:15:58 +0700

    add cass leftoper

--- CAPTCHA/captcha/spec/captcha.spec.js
+++ CAPTCHA/captcha/spec/captcha.spec.js
@@ -13,18 +13,102 @@ function captcha(var PatternValue,var OperatorValue,var LeftoperandValue,var Rig
 
 
 function operator(OperatorValue) {
-    if (OperatorValue == 1){
+    if (OperatorValue === 1){
       return "+";
     }
-    if (OperatorValue == 2){
+    if (OperatorValue === 2){
       return "-";
     }
 }
 
 
 
-function leftoperand() {
+function leftoperand(PatternValue,LeftoperandValue) {
+    if(PatternValue === 1 ){
+      if(LeftoperandValue === 0){
+        left = "0";
+        return left;
+      }
+      else if(LeftoperandValue === 1){
+        left = "1";
+        return left;
+      }
+      else if(LeftoperandValue === 2){
+        left = "2";
+        return left;
+      }
+      else if(LeftoperandValue === 3){
+        left = "3";
+        return left;
+      }
+      else if(LeftoperandValue === 4){
+        left = "4";
+        return left;
+      }
+      else if(LeftoperandValue === 5){
+        left = "5";
+        return left;
+      }
+      else if(LeftoperandValue === 6){
+        left = "6";
+        return left;
+      }
+      else if(LeftoperandValue === 7){
+        left = "7";
+        return left;
+      }
+      else if(LeftoperandValue === 8){
+        left = "8";
+        return left;
+      }
+      else if(LeftoperandValue === 9){
+        left = "9";
+        return left;
+      }
+    }
 
+    if(PatternValue === 2 ){
+      if(LeftoperandValue === 0){
+        left = "zero";
+        return left;
+      }
+      else if(LeftoperandValue === 1){
+        left = "one";
+        return left;
+      }
+      else if(LeftoperandValue === 2){
+        left = "two";
+        return left;
+      }
+      else if(LeftoperandValue === 3){
+        left = "three";
+        return left;
+      }
+      else if(LeftoperandValue === 4){
+        left = "four";
+        return left;
+      }
+      else if(LeftoperandValue === 5){
+        left = "five";
+        return left;
+      }
+      else if(LeftoperandValue === 6){
+        left = "six";
+        return left;
+      }
+      else if(LeftoperandValue === 7){
+        left = "seven";
+        return left;
+      }
+      else if(LeftoperandValue === 8){
+        left = "eight";
+        return left;
+      }
+      else if(LeftoperandValue === 9){
+        left = "nine";
+        return left;
+      }
+    }
 
 }
 
